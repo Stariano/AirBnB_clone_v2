def75f0718aa2f3dcc144b690c722d3db3eb317c
Steve <Steve>
2023-07-12 14:52:28 +0000

    First edit

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,11 +3,10 @@
 
 sudo apt-get -y update
 sudo apt-get -y upgrade
-sudo apt install nginx
-mkdir -p data/web_static/releases/test /data/web_static/shared
+sudo apt-get -y install nginx
+sudo mkdir -p data/web_static/releases/test /data/web_static/shared
 echo "<h1>Hii</h1>" >> data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data/
 sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 sudo service nginx start
-
