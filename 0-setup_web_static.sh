f02f7a7daa5002d11fe90c895ea6b9e841a805fc
Steve <Steve>
2023-07-12 13:20:49 +0000

    Tryin

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# Bash script that sets up your web servers for the deployment of web_static
+
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt install nginx
+mkdir -p data/web_static/releases/test /data/web_static/shared
+echo "<h1>Hii</h1>" >> data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -hR ubuntu:ubuntu /data/
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo service nginx start
+
